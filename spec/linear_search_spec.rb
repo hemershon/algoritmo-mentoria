53fac314e7030d44cbd54e8acb043597ab20c370
Hemershon Silva <hemershon@gmail.com>
2021-10-28 05:05:01 -0300

    linear seach

--- spec/linear_search_spec.rb
+++ spec/linear_search_spec.rb
@@ -4,6 +4,6 @@ describe LinearSearch do
   it 'find the index' do
     array = [1, 3, 65, 4, 45, 98, 35, 22]
 
-    expect(LinearSearch.search(array, 98)).to eq 1
+    expect(LinearSearch.search(array, 98)).to eq 5
   end
 end
