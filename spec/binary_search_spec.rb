e3e7d5240fe992c5491746d62a8848a17e513e5b
Hemershon Silva <hemershon@gmail.com>
2021-11-03 04:59:50 -0300

    feat(fibonacci) create

--- spec/binary_search_spec.rb
+++ spec/binary_search_spec.rb
@@ -4,7 +4,7 @@ describe BinarySearch do
   it "Value in a Array" do
     array = (0..100).to_a
     n = array.length
-    BinarySearch.search(array, 0, n - 1, 30).should eq 30
-    BinarySearch.search(array, 0, n - 1, 1000).should eq -1 
+    BinarySearch.search(array, 0, n - 1, 30).expect eq 30
+    BinarySearch.search(array, 0, n - 1, 1000).expect eq -1 
   end
 end
